4931621876e8d103692e3e5e38b6e7d8d2710f3d
AmanD15 <amandhammani2@gmail.com>
2022-09-03 15:29:07 +0530

    Added task 1b code

--- Task-1b/receiver.c
+++ Task-1b/receiver.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv) {
 
-	char filename[51]; //buffer to receive filename
+	char filename[51*8]; //buffer to receive filename
 	char received_file[61] = "received_";
 	unsigned int content_length = 0;
 	clock_t t_recv;
@@ -11,17 +11,61 @@ int main(int argc, char **argv) {
 
 	// TODO: Establish your cache covert channel
 
+	map_handle_t *handle;	 // declaring a handle for file mapping
+	char *map;
+
+	map = (char *) map_file("map_tmp.txt", &handle);  // mapping a file in memory (virtual address space)
+
+  
+	// End TODO
+	
 	filename[0] = '\0';
 
 	/* TODO:
 	 * Receive the filename from the sender process over covert channel
 	 * Use filename buffer to store received file name
 	*/
+	uint32_t sequenceMask = ((uint32_t) 1<<6) - 1;
+	uint32_t expSequence = 0b101011;
+	uint32_t bitSequence = 0;
+	char* file_n;
+	while (1)
+	{
+		bool bitReceived = detect_bit(handle);
+		bitSequence = ((uint32_t) bitSequence<<1) | bitReceived;
+		if ((bitSequence & sequenceMask) == expSequence) {
+			int strike_zeros = 0;
+			for (int i = 0; i < MAX_BUFFER_LEN; i++) {
+				bool det = detect_bit(handle);
+				if (det) {
+					filename[i] = '1';
+					strike_zeros = 0;
+				} else {
+					filename[i] = '0';
+					if (++strike_zeros >= 8 && i % 8 == 0) {
+						break;
+					}
+				}
+			}
 
-	strcat(received_file, filename);
+			// Print out message
+			file_n = binary_to_string(filename);
+			break;
+		}
+	}	
+	// End TODO
+
+	strcat(received_file, file_n);
 
 	// TODO: Create a text file with the filename stored in received_file buffer in write mode.
-
+	printf("Opening %s\n",received_file);
+	FILE *fptr;
+	fptr = fopen(received_file, "w");
+	if (fptr == NULL) {
+		printf("Could not open %s\n",received_file);
+		exit(0);
+	}
+	// End TODO
 	t_recv = clock();
 
 	/* TODO:
@@ -29,8 +73,41 @@ int main(int argc, char **argv) {
 	 * Wrtie them into the file opened earlier
 	 * store the length of the file content in content_length variable
 	*/
+	char* msg;
+	bool set_bit = false;
+	bitSequence = 0;
+	msg[8] = '\0';
+	while (1)
+	{
+		if (set_bit == false){
+			bool bitReceived = detect_bit(handle);
+			bitSequence = ((uint32_t) bitSequence<<1) | bitReceived;
+			if ((bitSequence & sequenceMask) == expSequence) {
+				set_bit = true;
+			}
+		}
+		else
+		{
+			bool current_char = 0;
+			for (int i = 0; i < 8; i++) {
+				bool rec = detect_bit(handle);
+				current_char = (current_char | rec);
+				if (rec) {
+					msg[i] = '1';
+				}
+				else {
+					msg[i] = '0';
+				}
+			}
+			if (current_char == false) break;
+			content_length++;
+			fprintf(fptr,"%s",binary_to_string(msg));
+		}
+	}
 
+	unmap_file(handle); 
 
+	// End TODO
 
 
 	t_recv = clock() - t_recv;
--- Task-1b/sender.c
+++ Task-1b/sender.c
@@ -4,30 +4,81 @@
 
 int main(int argc, char **argv) {
 
-	char filename[51];
+	char filename[51], *p;
 	unsigned int file_size = 0;
 	clock_t t_send;
 	double trans_time;
 	double trans_rate;
 
 	// TODO: Establish your cache covert channel
+	
+	map_handle_t *handle;	 // declaring a handle for file mapping
+	char *map;
+
+	map = (char *) map_file("map_tmp.txt", &handle);  // mapping a file in memory (virtual address space)
+	
+	// End TODO
 
 	printf("File to be send: ");
 	fgets (filename, 50, stdin);
+	if ((p = strchr(filename,'\n')) != NULL) *p = '\0';
 
 	/* TODO:
 	 * Send filename to the receiver
 	 * Open text file in read mode.
 	 * Store the size of file in file_size variable.
 	*/
-
+	FILE* file_ptr;
+	file_ptr = fopen(filename, "r");
+	if (file_ptr == NULL) {
+		printf("Could not open %s\n",filename);
+		exit(0);
+	}
+	char *binary;
+	binary = string_to_binary(filename);
+	unsigned int bin_len = strlen(binary);
+	bool sequence[8] = {1,0,1,0,1,0,1,1};
+	for (int i = 0; i < 8; i++) {
+			send_bit(sequence[i], handle);
+	}
+	for (int ind = 0; ind < bin_len; ind++) {
+			if (binary[ind] == '0') {
+				send_bit(false, handle);
+			} else {
+				send_bit(true, handle);
+			}
+	}
+	// Ensure channel is closed
+	for (int i = 0; i < 16; i++) {
+			send_bit(false, handle);
+	}
+	// End TODO
+	
 	t_send = clock();
 
 	// TODO: Transmit file contents to the Receiver over the cache covert channel
-
-
-
-
+	
+	for (int i = 0; i < 8; i++) {
+		send_bit(sequence[i], handle);
+	}
+	char ch[2];
+	while ((ch[0] = fgetc(file_ptr)) != EOF){
+		binary = string_to_binary(ch);
+		file_size++;
+		for (int ind = 0; ind < 8; ind++) {
+				if (binary[ind] == '0') {
+					send_bit(false, handle);
+				} else {
+					send_bit(true, handle);
+				}
+			}
+	}
+	file_size--;
+		
+	unmap_file(handle);
+	
+	// End TODO
+	
 	t_send = clock() - t_send;
 	trans_time = ((double) t_send) / CLOCKS_PER_SEC;
 	trans_rate = (double) (file_size * 8) / trans_time;
